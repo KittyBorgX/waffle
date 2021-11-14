5e2d40d59a0dc8cfb568cbe81d967a202f28f151
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-11-14 19:57:20 +0530

    Trim new lines once more lol

--- install.py
+++ install.py
@@ -9,16 +9,13 @@ def write_to_rc(file_name):
         system(f'source {file_name}')
         print("Installed waffle in PATH. Run waffle -h to confirm the success of installation")
 
-
 if platform == "linux" or platform == "linux2":
-    # linux
     if (environ['SHELL'] == '/usr/bin/zsh'):
         linux_zsh = f"/home/{environ['USER']}/.zshrc"
         write_to_rc(linux_zsh)
     if (environ['SHELL'] == '/usr/bin/bash'):
         linux_bash = f"/home/{environ['USER']}/.bash_profile"
         write_to_rc(linux_bash)
-
 
 elif platform == "darwin":
     if (environ['SHELL'] == '/bin/zsh'):
@@ -30,4 +27,3 @@ elif platform == "darwin":
 
 elif platform == "win32":
     print("Unimplemented lol")
-    # Windows...
